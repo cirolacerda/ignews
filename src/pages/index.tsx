f1947071e8dfe7b0a13c6c42e458b7cc6bc41741
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2021-08-01 10:53:55 -0300

    Adicionar estilos globais no app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 
-import style from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title> Início | ig.news</title>
       </Head>
-      <h1 className={style.title}>
-        Hello <span>World</span>
+      <h1>
+        Hello World
       </h1>
     </>
   )
