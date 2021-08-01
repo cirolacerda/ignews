2ec75bef1407e628b03e873ca12691cc15c6ec73
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2021-08-01 19:03:24 -0300

    Finalizar componente Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@
 import styles from './home.module.scss'
 
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -19,6 +20,8 @@ export default function Home() {
             Get access to  all the publications <br />
             <span>for $9.90</span>
           </p>
+
+          <SubscribeButton/>
 
         </section>
 
