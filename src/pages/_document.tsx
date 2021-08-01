a4da6d49ac160415159dc04dbc931beaf2244441
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2021-07-31 23:29:53 -0300

    Inserir title dinâmico por página

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,19 @@
-import Document, { Html, Head, Main, NextScript} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
                 <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
-                 
-                    <title>Ig.news</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+
                 </Head>
                 <body>
-                    <Main/>
-                    <NextScript/>
-                </body>               
+                    <Main />
+                    <NextScript />
+                </body>
             </Html>
 
         )
