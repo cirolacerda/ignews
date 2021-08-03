dc97d995e7125cb2ba527f738eefeba9f25cde8b
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2021-08-03 13:55:15 -0300

    Salvar usuário no banco de dados FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+import { faunadb } from '../../../services/faunadb'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -10,8 +13,35 @@ export default NextAuth({
       scope: 'read:user'
     }),
     // ...add more providers here
+
   ],
 
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await faunadb.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+  
+        return true
+        
+      } catch  {
+
+        return false
+        
+      }
+    },
+  }
+
   // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+
 })
